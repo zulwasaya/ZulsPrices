7f7b9a3e662a891571513f06a79d452509176229
Zul Wasaya <zul.wasaya@gmail.com>
2013-11-17 04:04:22 +0000

    added jumper to validation in repairs model

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -1,6 +1,6 @@
 class Repair < ActiveRecord::Base
   attr_accessible :comment, :garment, :item, :price
   validates_presence_of :garment, :item
-  validates_inclusion_of :garment, :in => %w(badge blouse coat curtain dress jacket shirt skirt trouser),
-                         :message => "values should be (badge blouse coat curtain dress jacket shirt skirt trouser)"
+  validates_inclusion_of :garment, :in => %w(badge blouse coat curtain dress jacket jumper shirt skirt trouser),
+                         :message => "values should be (badge blouse coat curtain dress jacket jumper shirt skirt trouser)"
 end
